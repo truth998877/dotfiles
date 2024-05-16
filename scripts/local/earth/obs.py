721cfecc0b71b5a2a8a59300f5a6b780fdf89973
Tony Ruther <tony.ruther@googlemail.com>
2024-05-16 21:38:13 +0100

    obs.py to take cli arguments

--- scripts/local/earth/obs.py
+++ scripts/local/earth/obs.py
@@ -7,11 +7,11 @@ parameters = simpleobsws.IdentificationParameters(ignoreNonFatalRequestChecks = 
 
 ws = simpleobsws.WebSocketClient(url = 'ws://localhost:4444', password = 'test', identification_parameters = parameters) # Every possible argument has been passed, but none are required. See lib code for defaults.
 
-async def make_request():
+async def make_request(Request):
     await ws.connect() # Make the connection to obs-websocket
     await ws.wait_until_identified() # Wait for the identification handshake to complete
 
-    request = simpleobsws.Request('GetVersion') # Build a Request object
+    request = simpleobsws.Request(Request) # Build a Request object
 
     ret = await ws.call(request) # Perform the request
     if ret.ok(): # Check if the request succeeded
@@ -19,5 +19,14 @@ async def make_request():
 
     await ws.disconnect() # Disconnect from the websocket server cleanly
 
+parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument('request')
+
+args = parser.parse_args()
+
 loop = asyncio.get_event_loop()
-loop.run_until_complete(make_request())
+loop.run_until_complete(make_request(args.request))
